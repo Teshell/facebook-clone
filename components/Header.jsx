234da724a8711b73017c9edf3afbf6518e4d6cf2
Teshell <fayrongeek@gmail.com>
2022-03-29 09:50:52 +0100

    Header done

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,7 +19,7 @@ import HeaderIcon from "./HeaderIcon";
 
 const Header = () => {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
@@ -32,7 +32,7 @@ const Header = () => {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook..."
           />
@@ -40,8 +40,8 @@ const Header = () => {
       </div>
 
       <div className="flex justify-center flex-grow">
-        <div className="flex">
-          <HeaderIcon Icon={HomeIcon} />
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} active />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
           <HeaderIcon Icon={ShoppingCartIcon} />
@@ -49,7 +49,15 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+
+        <p className="whitespace-nowrap font-semibold pr-3">Zinedine Hamadi</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 };
